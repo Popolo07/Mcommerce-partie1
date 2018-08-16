047af7dec5fff3a14981edc5aff8b49b1aaca083
Popolo07 <paul.arich@yahoo.fr>
2018-08-16 19:45:07 +0200

    test commit

--- microcommerce/src/main/java/com/ecommerce/microcommerce/controller/ProduitController.java
+++ microcommerce/src/main/java/com/ecommerce/microcommerce/controller/ProduitController.java
@@ -6,8 +6,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
--- microcommerce/src/main/java/com/ecommerce/microcommerce/exceptions/ProduitIntrouvableException.java
+++ microcommerce/src/main/java/com/ecommerce/microcommerce/exceptions/ProduitIntrouvableException.java
@@ -3,6 +3,7 @@ package com.ecommerce.microcommerce.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@SuppressWarnings("serial")
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProduitIntrouvableException extends Exception {
 
